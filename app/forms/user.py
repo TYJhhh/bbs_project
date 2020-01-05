ba199681dfee6c073c4f69522402ad73d52ff4e2
runoob <1227794413@qq.com>
2020-01-05 17:20:27 +0800

    2020/1/5 role before!

--- app/forms/user.py
+++ app/forms/user.py
@@ -1,14 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from app.extensions import photos
+from app.models import User
 class RegisterForm(FlaskForm):
     username = StringField('用户名', validators=[DataRequired(), Length(6, 30, message="用户名长度不符合要求")])
     password = PasswordField('密码', validators=[DataRequired(), Length(6, 30, message="密码长度不符合要求")])
     confirm = PasswordField('确认密码', validators=[EqualTo('password', message="两次密码不一致")])
     email = StringField('邮箱', validators=[Email(message="邮箱格式不正确")])
     submit = SubmitField("立即注册")
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('该邮箱已被注册')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('该用户名已被注册')
 
 class LoginForm(FlaskForm):
     username = StringField('用户名', validators=[DataRequired(), Length(6, 30, message="用户名长度不符合要求")])
